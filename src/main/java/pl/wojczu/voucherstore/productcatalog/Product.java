e340e0e0d0cf3536aa3d0d0ddff10480a0f07650
Wojciech Czuma <wojciech.czuma@gmail.com>
2020-12-03 20:04:03 +0100

    introduce jpa repo as product storage

--- src/main/java/pl/wojczu/voucherstore/productcatalog/Product.java
+++ src/main/java/pl/wojczu/voucherstore/productcatalog/Product.java
@@ -1,17 +1,23 @@
 package pl.wojczu.voucherstore.productcatalog;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Entity
 public class Product {
-    private final UUID productId;
 
+    @Id
+    private String productId;
     private String description;
     private String image;
     private BigDecimal price;
 
+    Product(){}
+
     public Product(UUID productId) {
-        this.productId = productId;
+        this.productId = productId.toString();
 
     }
 
--- src/main/java/pl/wojczu/voucherstore/productcatalog/ProductStorage.java
+++ src/main/java/pl/wojczu/voucherstore/productcatalog/ProductStorage.java
@@ -1,12 +1,19 @@
 package pl.wojczu.voucherstore.productcatalog;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductStorage {
+@org.springframework.stereotype.Repository
+public interface ProductStorage extends Repository<Product, String> {
     void save(Product newProduct);
 
-    Optional<Product> getById(String productId);
+    @Query("SELECT p FROM Product p WHERE p.productId = :productId")
+    Optional<Product> getById(@Param("productId") String productId);
 
+    @Query("SELECT p FROM Product p WHERE p.price IS NOT NULL AND p.description IS NOT NULL")
     List<Product> allPublishedProducts();
 }
