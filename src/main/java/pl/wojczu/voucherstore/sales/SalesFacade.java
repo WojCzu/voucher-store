322e5661df0159835d5d32c022d4bcf68be759a2
Wojciech Czuma <wojciech.czuma@gmail.com>
2021-01-13 19:44:58 +0100

    add app layer for basket

--- src/main/java/pl/wojczu/voucherstore/sales/SalesFacade.java
+++ src/main/java/pl/wojczu/voucherstore/sales/SalesFacade.java
@@ -1,7 +1,31 @@
 package pl.wojczu.voucherstore.sales;
 
-public class SalesFacade {
-    public void addProduct(String productId) {
+import pl.wojczu.voucherstore.productcatalog.Product;
+import pl.wojczu.voucherstore.productcatalog.ProductCatalogFacade;
 
+public class SalesFacade {
+    ProductCatalogFacade productCatalogFacade;
+    InMemoryBasketStorage basketStorage;
+    CurrentCustomerContext currentCustomerContext;
+    Inventory inventory;
+
+    public SalesFacade(ProductCatalogFacade productCatalogFacade, InMemoryBasketStorage basketStorage, CurrentCustomerContext currentCustomerContext, Inventory inventory) {
+        this.productCatalogFacade = productCatalogFacade;
+        this.basketStorage = basketStorage;
+        this.currentCustomerContext = currentCustomerContext;
+        this.inventory = inventory;
+    }
+
+    public void addProduct(String productId) {
+        Product product = productCatalogFacade.getById(productId);
+        Basket basket = basketStorage.loadForCustomer(getCurrentCustomerId())
+                .orElse(Basket.empty());
+
+        basket.add(product, inventory);
+        basketStorage.addForCustomer(getCurrentCustomerId(), basket);
+    }
+
+    private String getCurrentCustomerId(){
+        return currentCustomerContext.getCurrentCustomerId();
     }
 }
