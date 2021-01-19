8f7b569cc0b4d9826aa82fa9f569556cf571e926
Wojciech Czuma <wojciech.czuma@gmail.com>
2021-01-19 12:01:53 +0100

    expose sales module via http

---
+++ src/main/java/pl/wojczu/voucherstore/sales/SalesConfiguration.java
@@ -0,0 +1,40 @@
+package pl.wojczu.voucherstore.sales;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pl.wojczu.voucherstore.productcatalog.ProductCatalogFacade;
+import pl.wojczu.voucherstore.sales.basket.InMemoryBasketStorage;
+import pl.wojczu.voucherstore.sales.offer.OfferMaker;
+import pl.wojczu.voucherstore.sales.product.ProductCatalogProductDetailsProvider;
+import pl.wojczu.voucherstore.sales.product.ProductDetailsProvider;
+
+import java.util.UUID;
+
+@Configuration
+public class SalesConfiguration {
+
+    @Bean
+    SalesFacade salesFacade(ProductCatalogFacade productCatalogFacade, OfferMaker offerMaker, Inventory inventory){
+        return new SalesFacade(
+                productCatalogFacade,
+                new InMemoryBasketStorage(),
+                () -> UUID.randomUUID().toString(),
+                inventory,
+                offerMaker
+
+        );
+    }
+    @Bean
+    Inventory inventory(){
+        return (productId -> true);
+    }
+    @Bean
+    OfferMaker offerMaker(ProductDetailsProvider productDetailsProvider){
+        return new OfferMaker(productDetailsProvider);
+    }
+
+    @Bean
+    ProductDetailsProvider productDetailsProvider(ProductCatalogFacade productCatalogFacade){
+        return new ProductCatalogProductDetailsProvider(productCatalogFacade);
+    }
+}
